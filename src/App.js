2e2b3d4ec1126547d0f7942df0a3d5c47c59e280
Anis-Ghliss <ghlissanis@gmail.com>
2020-07-17 11:30:56 +0200

    netlify config

--- src/App.js
+++ src/App.js
@@ -1,47 +1,53 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import "react-bootstrap-typeahead/css/Typeahead.css";
-import './App.css';
-import Movie from './components/Movies'
-import Tvshow from './components/TvShows'
-import Nav from './components/Nave'
-import Details from './components/Details'
-import Apikey from './apkiKeys'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-
+import "./App.css";
+import Movie from "./components/Movies";
+import Tvshow from "./components/TvShows";
+import Nav from "./components/Nave";
+import Details from "./components/Details";
+import Apikey from "./apkiKeys";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [tvShows, setTvShows] = useState([]);
 
-  const [movies,setMovies] = useState([])
-  const [tvShows,setTvShows] = useState([])
-
-  useEffect(()=>{
-    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${Apikey.TMDBKEY}`)
-      .then( response => response.json())
-      .then(data => {
-        setMovies(data["results"])
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${Apikey.TMDBKEY}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setMovies(data["results"]);
       })
-      .catch(console.log())
+      .catch(console.log());
 
-      fetch(`https://api.themoviedb.org/3/tv/popular?api_key=${Apikey.TMDBKEY}`)
-    .then( response => response.json())
-      .then(data => {
-        setTvShows(data["results"])
+    fetch(`https://api.themoviedb.org/3/tv/popular?api_key=${Apikey.TMDBKEY}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setTvShows(data["results"]);
       })
-      .catch(console.log())
-    }, [])
-
+      .catch(console.log());
+  }, []);
 
   return (
-  <Router>
-    <div className="App">
-      <Nav/>
-      <Switch>
-        <Route exact path="/" render={ (props) => ( <Movie {...props} movies={ movies} /> ) }/>
-        <Route exact path="/tvshows" render={ (props) => ( <Tvshow {...props} tvshows={ tvShows} /> ) }/>
-        <Route exact path='/details/:id/:type' component={ Details}/>
-      </Switch>
-    </div>
-  </Router>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route
+            path="/"
+            render={(props) => <Movie {...props} movies={movies} />}
+          />
+          <Route
+            exact
+            path="/tvshows"
+            render={(props) => <Tvshow {...props} tvshows={tvShows} />}
+          />
+          <Route exact path="/details/:id/:type" component={Details} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
